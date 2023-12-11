62e8ac5bfd9cfe7a67c8ef3bb24f5ed645c6c2f8
rafli.haidar30@gmail.com <rafli.haidar30@gmail.com>
2023-12-11 19:29:39 +0700

    fixed bug in distribbutor controller

--- src/controller/DistributorController.java
+++ src/controller/DistributorController.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-import javax.swing.JTable;
-import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import java.sql.*;
 import javax.swing.table.DefaultTableModel;
-import model.ComboBoxModel;
 import model.DistributorModel;
 
 /**
